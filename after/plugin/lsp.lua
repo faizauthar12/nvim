5e3eab28f35b8246bfa96b0657bc6a84a030b088
Faiz Authar <faizauthar@gmail.com>
2023-02-03 14:17:08 +0700

    plugin: lsp use ctrl+space for autocompletions menu

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,12 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+local cmp = require('cmp')
+
+lsp.setup_nvim_cmp({
+    mapping = lsp.defaults.cmp_mappings({
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
+})
+
 lsp.setup()
