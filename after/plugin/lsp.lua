cf5d0dd21e1aa39b7c20a959b252880d2bbfea7c
Faiz Authar <faizauthar@gmail.com>
2023-02-08 17:45:39 +0700

    Setup CMP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,6 +3,12 @@ lsp.preset('recommended')
 
 local cmp = require('cmp')
 
+cmp.setup({
+    sources = {
+        name = 'path'
+    }
+})
+
 lsp.setup_nvim_cmp({
     mapping = lsp.defaults.cmp_mappings({
         ['<C-Space>'] = cmp.mapping.complete(),
