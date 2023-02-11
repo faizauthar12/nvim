6c8a6035d22e509b3a6a33077aa1299cd73cd6e4
Faiz Authar <faizauthar@gmail.com>
2023-02-11 19:05:04 +0700

    lsp: adjust keyword_length

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,16 +3,16 @@ lsp.preset('recommended')
 
 local cmp = require('cmp')
 
-cmp.setup({
-    sources = {
-        name = 'path'
-    }
-})
-
 lsp.setup_nvim_cmp({
     mapping = lsp.defaults.cmp_mappings({
         ['<C-Space>'] = cmp.mapping.complete(),
-    })
+    }),
+    sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp', keyword_length = 1},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luaship', keyword_length = 2},
+    }
 })
 
 lsp.setup()
