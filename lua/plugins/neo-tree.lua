038b0ebf6493554b5c8465b8ee3221fda84255a7
Faiz Authar <faizauthar@gmail.com>
2024-07-18 10:27:46 +0700

    stash

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+-- Nicer filetree than NetRW
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				window = {
+					position = "right",
+				},
+			})
+			require("helpers.keys").map(
+				{ "n", "v" },
+				"<leader>e",
+				"<cmd>NeoTreeRevealToggle<cr>",
+				"Toggle file explorer"
+			)
+		end,
+	},
+}
---
+++ lua/plugins/wf.lua
@@ -0,0 +1,9 @@
+-- Wf replacement of which key
+return {
+    {
+        "Cassin01/wf.nvim",
+        config = function()
+            require("wf").setup()
+        end,
+    }
+}
