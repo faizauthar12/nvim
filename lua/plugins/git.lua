9e2159d1f84d1780857ddc593b5b1d57dd49a2a8
Faiz Authar <faizauthar@gmail.com>
2024-10-20 00:25:30 +0700

    nvim: git: add more shortcut such as git log status, diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -25,6 +25,9 @@ return {
             local map = require("helpers.keys").map
             map("n", "<leader>ga", "<cmd>Git add %<cr>", "Stage the current file")
             map("n", "<leader>gb", "<cmd>Git blame<cr>", "Show the blame")
+            map("n", "<leader>gd", "<cmd>Git diff<cr>", "Show git diff")
+            map("n", "<leader>gl", "<cmd>Git log<cr>", "Show git log")
+            map("n", "<leader>gs", "<cmd>Git status<cr>", "Show git status")
         end
     }
 }
