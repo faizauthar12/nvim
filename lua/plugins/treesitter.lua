fc7f9f5b98a7e08dd1ae05701a2f2b00bc0f37ad
Faiz Authar <faizauthar@gmail.com>
2026-04-16 15:36:50 +0700

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -33,6 +33,11 @@ return {
                     additional_vim_regex_highlighting = false,
                 },
             })
+
+            require('treesitter-context').setup {
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                multiwindow = true, -- Enable multiwindow support.
+            }
         end
     }
 }
